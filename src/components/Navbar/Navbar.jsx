749b4e45697b6dbf997e62c8cb9508c2241d692f
Aman <developer@codelinear.com>
2024-05-03 11:10:37 +0530

    final three changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,155 +1,229 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import "./nav.scss";
-import Img from "./logoo.png";
-import Image from "next/image";
-import Link from "next/link";
+'use client'
+import React, { useState, useEffect } from 'react'
+import './nav.scss'
+import Img from './logoo.png'
+import log from './logo-blue.png'
 
-function Navbar() {
-  const [isScrolled, setIsScrolled] = useState(false);
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { useRouter } from 'next/navigation'
+import { usePathname } from 'next/navigation'
+
+function Navbar () {
+  const [isScrolled, setIsScrolled] = useState(false)
 
   useEffect(() => {
     // if (typeof globalThis.window? !== "undefined") {
     const handleScroll = () => {
       if (globalThis.window?.scrollY > 100) {
-        setIsScrolled(true);
+        setIsScrolled(true)
       } else {
-        setIsScrolled(false);
+        setIsScrolled(false)
       }
-    };
+    }
 
     // Add scroll event listener
-    globalThis.window?.addEventListener("scroll", handleScroll);
+    globalThis.window?.addEventListener('scroll', handleScroll)
 
     // Clean up event listener on component unmount
     return () => {
-      globalThis.window?.removeEventListener("scroll", handleScroll);
-    };
+      globalThis.window?.removeEventListener('scroll', handleScroll)
+    }
     // }
-  }, []);
+  }, [])
 
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
-  let [width, setWidth] = useState(0);
+  const [isDropdownOpen, setDropdownOpen] = useState(false)
+  let [width, setWidth] = useState(0)
 
   useEffect(() => {
     // if (typeof window === "undefined") {
     const updateWidth = () => {
-      setWidth(globalThis.window?.innerWidth);
-    };
-    updateWidth();
-    globalThis.window?.addEventListener("resize", updateWidth);
+      setWidth(globalThis.window?.innerWidth)
+    }
+    updateWidth()
+    globalThis.window?.addEventListener('resize', updateWidth)
     return () => {
-      globalThis.window?.removeEventListener("resize", updateWidth);
-    };
+      globalThis.window?.removeEventListener('resize', updateWidth)
+    }
     // }
-  }, []);
+  }, [])
 
   const toggleDropdown = () => {
-    setDropdownOpen(!isDropdownOpen);
-  };
-  let [active, setactive] = useState(false);
+    setDropdownOpen(!isDropdownOpen)
+  }
+  let [active, setactive] = useState(false)
   const hamburger = () => {
-    setactive(!active);
-  };
+    setactive(!active)
+  }
+
+  // const router = useRouter()
+
+  const currentRoutee = usePathname() // Get the current path
+  // const isContactPage = currentRoutee === '/contactus'
+
+  const navClass =
+    currentRoutee === '/contactus'
+      ? 'flex items-center text-black text-xs mt-1 mb-6 gap-8 py-[10px]'
+      : 'flex items-center text-white text-xs mt-1 mb-6 gap-8 py-[10px]'
+
   return (
     <>
       {width <= 900 ? (
         <>
-          <header className="bg[#062b43] bg-opacity-50 w-full mx-auto sticky top-0 h[100px] rounded-full z-[9990] h-[76px]">
+          <header className='bg[#062b43] bg-opacity-50 w-full mx-auto sticky top-0 h[100px] rounded-full z-[9990] h-[76px]'>
             <div
               className={
                 isScrolled
-                  ? "bg-[#1d83b3b3] w-full  flex justify-between font-openSans z-[1] sticky top-0  h-[100px] mt8 "
-                  : "w-full  flex justify-between font-openSans z-[1] sticky -top-8 h-[80px] "
+                  ? 'bg-[#1d83b3b3] w-full  flex justify-between font-openSans z-[1] sticky top-0  h-[100px] mt8 '
+                  : 'w-full  flex justify-between font-openSans z-[1] sticky -top-8 h-[80px] '
               }
             >
-              <div className="logo">
-                <Link href="/" className="mt-1 ml-[10px] mb-2 h-full">
-                  <Image src={Img} alt="" className="mix-blend-differene" />
+              <div className='logo'>
+                <Link href='/' className='mt-1 ml-[10px] mb-2 h-full'>
+                  {currentRoutee === '/contactus' ? (
+                    // <Image src={log} alt='' className='mix-blend-differene' />
+                    <>
+                      {isScrolled ? (
+                        <Image
+                          src={Img}
+                          alt=''
+                          className='mix-blend-differene'
+                        />
+                      ) : (
+                        <Image
+                          src={log}
+                          alt=''
+                          className='mix-blend-differene'
+                        />
+                      )}
+                    </>
+                  ) : (
+                    <Image src={Img} alt='' className='mix-blend-differene' />
+                  )}
                 </Link>
               </div>
               <button onClick={hamburger}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="65"
-                  height="42"
-                  viewBox="0 0 65 42"
-                  fill="#fff"
-                >
-                  <line
-                    x1="13.4353"
-                    y1="12.1875"
-                    x2="51.5647"
-                    y2="12.1875"
-                    stroke="white"
-                  />
-                  <line
-                    x1="13.4353"
-                    y1="20.3594"
-                    x2="51.5647"
-                    y2="20.3594"
-                    stroke="white"
-                  />
-                  <line
-                    x1="13.4353"
-                    y1="28.813"
-                    x2="51.5647"
-                    y2="28.813"
-                    stroke="white"
-                  />
-                </svg>
+                {currentRoutee === '/contactus' ? (
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='65'
+                    height='42'
+                    viewBox='0 0 65 42'
+                    fill='#000'
+                  >
+                    <line
+                      x1='13.4353'
+                      y1='12.1875'
+                      x2='51.5647'
+                      y2='12.1875'
+                      stroke='#000'
+                    />
+                    <line
+                      x1='13.4353'
+                      y1='20.3594'
+                      x2='51.5647'
+                      y2='20.3594'
+                      stroke='#000'
+                    />
+                    <line
+                      x1='13.4353'
+                      y1='28.813'
+                      x2='51.5647'
+                      y2='28.813'
+                      stroke='#000'
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='65'
+                    height='42'
+                    viewBox='0 0 65 42'
+                    fill='#fff'
+                  >
+                    <line
+                      x1='13.4353'
+                      y1='12.1875'
+                      x2='51.5647'
+                      y2='12.1875'
+                      stroke='white'
+                    />
+                    <line
+                      x1='13.4353'
+                      y1='20.3594'
+                      x2='51.5647'
+                      y2='20.3594'
+                      stroke='white'
+                    />
+                    <line
+                      x1='13.4353'
+                      y1='28.813'
+                      x2='51.5647'
+                      y2='28.813'
+                      stroke='black'
+                    />
+                  </svg>
+                )}
               </button>
 
               <div
                 className={`lists flex items-center justify-between flex-col ${
-                  active ? "active " : ""
+                  active ? 'active ' : ''
                 }`}
               >
-                <nav className="flex gap-6 text-base font-medium text-black items-center flex-col w-1/2 mt-20">
+                <nav className='flex gap-6 text-base font-medium text-black items-center flex-col w-1/2 mt-20'>
                   <Link
                     onClick={hamburger}
-                    className="hovertext-[#733e3d] text-white"
-                    href="/About"
+                    className='hovertext-[#733e3d] text-white'
+                    href='/About'
                   >
                     ABOUT US
                   </Link>
-                  <Link
+                  {/* <Link
                     onClick={hamburger}
-                    className="hovertext-[#733e3d]  text-white"
-                    href="/job-seeker"
+                    className='hovertext-[#733e3d]  text-white'
+                    href='/job-seeker'
                   >
                     JOBSEEKER
+                  </Link> */}
+                  <Link
+                    onClick={hamburger}
+                    className='hovertext-[#733e3d]  text-white'
+                    href='/services'
+                  >
+                    SERVICES
                   </Link>
                   <Link
                     onClick={hamburger}
-                    className="hovertext-[#733e3d]  text-white"
-                    href="/employee"
+                    className='hovertext-[#733e3d]  text-white'
+                    href='/employee'
                   >
-                    EMPLOYER
+                    STAFF AUGMENTATION
                   </Link>
 
                   <Link
                     onClick={hamburger}
-                    className="hovertext-[#733e3d] text-white"
-                    href="/contactus"
+                    className='hovertext-[#733e3d] text-white'
+                    href='/contactus'
                   >
                     CONTACT
                   </Link>
                 </nav>
-                <div className="absolute right-20 top-20">
+                <div className='absolute right-20 top-20'>
                   <button
-                    type="reset"
-                    className="bg-gray-300 text-gray-800 py-2 px-4 rounded-md ml-2"
+                    type='reset'
+                    className='bg-gray-300 text-gray-800 py-2 px-4 rounded-md ml-2'
                     onClick={hamburger}
                   >
                     <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="#fff"
+                      xmlns='http://www.w3.org/2000/svg'
+                      width='24'
+                      height='24'
+                      viewBox='0 0 24 24'
+                      fill='#fff'
                     >
-                      <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
+                      <path d='M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z' />
                     </svg>
                   </button>
                 </div>
@@ -161,44 +235,80 @@ function Navbar() {
         <div
           className={
             isScrolled
-              ? "bg-[#1d83b3b3] w-full  flex justify-between font-openSans z-[1] sticky top-0  h-[80px] mt8 "
-              : "w-full  flex justify-between font-openSans z-[1] sticky top-3 h-[80px] "
+              ? 'bg-[#1d83b3b3] w-full !text-white  flex justify-between font-openSans z-[1] sticky top-0  h-[80px] mt8 '
+              : 'w-full  flex justify-between font-openSans z-[1] sticky top-3 h-[80px] '
           }
         >
-          <Link href="/" className="mt-1 ml-[10px] mb-2 h-full">
-            <Image src={Img} alt="" className="mix-blend-differene" />
+          <Link href='/' className='mt-1 ml-[10px] mb-2 h-full'>
+            {currentRoutee === '/contactus' ? (
+              // <Image src={log} alt='' className='mix-blend-differene' />
+              <>
+                {isScrolled ? (
+                  <Image src={Img} alt='' className='mix-blend-differene' />
+                ) : (
+                  <Image src={log} alt='' className='mix-blend-differene' />
+                )}
+              </>
+            ) : (
+              <Image src={Img} alt='' className='mix-blend-differene' />
+            )}
           </Link>
 
-          <div className="mr-11">
-            <ul className="flex items-center text-white text-xs mt-1 mb-6 gap-8 py-[10px]">
-              <Link href="/About">
-                <li className="hover:rounded-full hover:border px-[14px] py-2">
+          <div className='mr-11'>
+            <ul className={navClass}>
+              <Link href='/About'>
+                <li
+                  className={
+                    isScrolled
+                      ? 'hover:rounded-full hover:border px-[14px] py-2 text-[#fff]'
+                      : 'hover:rounded-full hover:border px-[14px] py-2'
+                  }
+                >
                   ABOUT US
                 </li>
               </Link>
-              <Link href="/job-seeker">
-                <li className="hover:rounded-full hover:border px-[14px] py-2">
-                  JOBSEEKER
-                </li>
-              </Link>
-              <Link href="/employee">
-                <li className="hover:rounded-full hover:border px-[14px] py-2">
-                  EMPLOYER
-                </li>
-              </Link>
-              <Link href="/services">
-                <li className="hover:rounded-full hover:border px-[14px] py-2">
+              <Link href='/services'>
+                <li
+                  className={
+                    isScrolled
+                      ? 'hover:rounded-full hover:border px-[14px] py-2 text-[#fff]'
+                      : 'hover:rounded-full hover:border px-[14px] py-2'
+                  }
+                >
                   SERVICES
                 </li>
               </Link>
-              <Link href="/contactus">
-                <li className="group relative rounded-full border px-[14px] py-2">
+              {/* <Link href="/job-seeker">
+                <li className="hover:rounded-full hover:border px-[14px] py-2">
+                  JOBSEEKER
+                </li>
+              </Link> */}
+              <Link href='/employee'>
+                <li
+                  className={
+                    isScrolled
+                      ? 'hover:rounded-full hover:border px-[14px] py-2 text-[#fff]'
+                      : 'hover:rounded-full hover:border px-[14px] py-2'
+                  }
+                >
+                  STAFF AUGMENTATION
+                </li>
+              </Link>
+
+              <Link href='/contactus'>
+                <li
+                  className={
+                    isScrolled
+                      ? 'group relative rounded-full border px-[14px] py-2 text-[#fff]'
+                      : 'group relative rounded-full border px-[14px] py-2'
+                  }
+                >
                   CONTACT
                   {/* <!-- Dropdown that appears on hover --> */}
-                  <div className="absolute top-0 pt-10 left-0 mt-1 bgwhite text-black hidden group-hover:block rounded shadow-lg">
+                  <div className='absolute top-0 pt-10 left-0 mt-1 bgwhite text-black hidden group-hover:block rounded shadow-lg'>
                     <ul>
-                      <li className="px-[14px] py-2 rounded-full  bg-gray-200">
-                        <Link href="/job-seeker">CAREERS</Link>
+                      <li className='px-[14px] py-2 rounded-full  bg-gray-200'>
+                        <Link href='/job-seeker'>CAREERS</Link>
                       </li>
                     </ul>
                   </div>
@@ -209,7 +319,7 @@ function Navbar() {
         </div>
       )}
     </>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
--- src/components/aboutpage/Expertise.jsx
+++ src/components/aboutpage/Expertise.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react'
 
 const Expertise = () => {
   return (
     <>
-      <section className="py-8">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="text-center mb-20">
-            <h2 className="text-5xl font-bold">Expertise</h2>
-            <p className="mt-4 text-xl text-gray-600">
+      <section className='py-8'>
+        <div className='max-w-6xl mx-auto px-4 sm:px-6 lg:px-8'>
+          <div className='text-center mb-20'>
+            <h2 className='text-5xl font-bold'>Expertise</h2>
+            <p className='mt-4 text-xl text-gray-600'>
               Our contractors and direct hire candidates are not only
               specialized in specific technologies and job functions, but are
               also experienced in individual industries to respond to the unique
@@ -16,102 +16,102 @@ const Expertise = () => {
             </p>
           </div>
 
-          <h3 className="text-3xl font-bold my-10">IT Services</h3>
+          <h3 className='text-3xl font-bold my-10'>IT</h3>
 
-          <div className="flex max-md:flex-wrap gap-10 text-left">
-            <div className="flex flex-col gap-8 ">
-              <div className="flex justify-start items-start  gap-3 ">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+          <div className='flex max-md:flex-wrap gap-10 text-left'>
+            <div className='flex flex-col gap-8 '>
+              <div className='flex justify-start items-start  gap-3 '>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Software &amp; Web Development/Engineering
                 </h3>
               </div>
-              <div className="flex justify-start items-start gap-3 ">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <div className='flex justify-start items-start gap-3 '>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Application Design, Development and Maintenance
                 </h3>
               </div>
-              <div className="flex justify-start items-start gap-3">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <div className='flex justify-start items-start gap-3'>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Database Design, Development and Administration
                 </h3>
-              </div>{" "}
+              </div>{' '}
             </div>
-            <div className="flex flex-col gap-8 ">
-              <div className="flex justify-start items-start gap-3">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+            <div className='flex flex-col gap-8 '>
+              <div className='flex justify-start items-start gap-3'>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Enterprise Systems Analysis and Integration
                 </h3>
               </div>
-              <div className="flex justify-start items-start gap-3">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <div className='flex justify-start items-start gap-3'>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Business Analysis
                 </h3>
               </div>
-              <div className="flex justify-start items-start gap-3">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <div className='flex justify-start items-start gap-3'>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Project Management
                 </h3>
               </div>
             </div>
 
-            <div className="flex flex-col gap-8 ">
-              <div className="flex justify-start items-start gap-3">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+            <div className='flex flex-col gap-8 '>
+              <div className='flex justify-start items-start gap-3'>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Quality Assurance &amp; Testing
                 </h3>
               </div>
-              <div className="flex justify-start items-start gap-3">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <div className='flex justify-start items-start gap-3'>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Network Engineering
                 </h3>
               </div>
-              <div className="flex justify-start items-start gap-3">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <div className='flex justify-start items-start gap-3'>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Infrastructure Support
                 </h3>
               </div>
-              <div className="flex justify-start items-start gap-3">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <div className='flex justify-start items-start gap-3'>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Technical Support
                 </h3>
               </div>
             </div>
           </div>
 
-          <h3 className="text-3xl font-bold my-10">NON- IT Services</h3>
-          <div className="flex max-md:flex-wrap gap-10 text-left">
-            <div className="flex max-md:flex-wrap gap-8 ">
-              <div className="flex justify-start items-start  gap-3 ">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+          <h3 className='text-3xl font-bold my-10'>Non-IT</h3>
+          <div className='flex max-md:flex-wrap gap-10 text-left'>
+            <div className='flex max-md:flex-wrap gap-8 '>
+              <div className='flex justify-start items-start  gap-3 '>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Finance and Accounting
                 </h3>
               </div>
-              <div className="flex justify-start items-start gap-3 ">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <div className='flex justify-start items-start gap-3 '>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Design
                 </h3>
               </div>
-              <div className="flex justify-start items-start gap-3">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <div className='flex justify-start items-start gap-3'>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Healthcare
                 </h3>
               </div>
-              <div className="flex justify-start items-start gap-3">
-                <CheckCircleIcon className="mb-3 inline-block h-6 w-6 text-blue-500" />
-                <h3 className="text-lg leading-6 font-medium text-gray-900">
+              <div className='flex justify-start items-start gap-3'>
+                <CheckCircleIcon className='mb-3 inline-block h-6 w-6 text-blue-500' />
+                <h3 className='text-lg leading-6 font-medium text-gray-900'>
                   Legal
                 </h3>
               </div>
@@ -120,27 +120,27 @@ const Expertise = () => {
         </div>
       </section>
     </>
-  );
-};
+  )
+}
 
-export default Expertise;
+export default Expertise
 
-function CheckCircleIcon(props) {
+function CheckCircleIcon (props) {
   return (
     <svg
       {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     >
-      <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
-      <polyline points="22 4 12 14.01 9 11.01" />
+      <path d='M22 11.08V12a10 10 0 1 1-5.93-9.14' />
+      <polyline points='22 4 12 14.01 9 11.01' />
     </svg>
-  );
+  )
 }
